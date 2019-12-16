6ef978e262a31df22ab3f8e67791dd461f378a00
sam0l <58955966+sam0l@users.noreply.github.com>
2019-12-17 04:25:29 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+/* var: declares a  variable, optionally initializaing it to a value. */
+/* let: declares a block-scoped, local variable, optionally initializing it to a value. */
+/* const: declares a block-scoped, read-only named constant. */
+
